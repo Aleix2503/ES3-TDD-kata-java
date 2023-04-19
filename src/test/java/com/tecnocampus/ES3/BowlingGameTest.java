0333cccee9d2ce7c8ca2695121589f80e913cb74
Aleix Muñoz Rodriguez <amunozr@edu.tecnocampus.cat>
2023-04-19 13:36:04 +0200

    commit 40

--- src/test/java/com/tecnocampus/ES3/BowlingGameTest.java
+++ src/test/java/com/tecnocampus/ES3/BowlingGameTest.java
@@ -35,7 +35,7 @@ public class BowlingGameTest {
     }
 
     @Test public void one_strike() {
-        g.roll(10); // strike
+        rollStrike();
         g.roll(3);
         g.roll(4);
         rollMany(16, 0);
@@ -50,6 +50,10 @@ public class BowlingGameTest {
     private void rollSpare() {
         g.roll(5);
         g.roll(5);
+    }
+
+    private void rollStrike() {
+        g.roll(10);
     }
 
 }
